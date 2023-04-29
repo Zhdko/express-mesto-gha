c1f9d306f926048e9a89bc0c5eb95f5d0b46ff00
Alexandr Zhdanko <zhdko@yandex.ru>
2023-04-29 22:30:01 +0400

    fix: исправление ошибки при добавлении карточки

--- middlewares/cardValidation.js
+++ middlewares/cardValidation.js
@@ -3,7 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 const validateCreateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().requaired().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
+    link: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
   }),
 });
 
