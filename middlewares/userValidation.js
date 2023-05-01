88042be8aab0cf09ce5b16350518d0f5eff3bcae
Alexandr Zhdanko <zhdko@yandex.ru>
2023-05-01 17:39:00 +0400

    fix: исправление валидации

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -26,14 +26,14 @@ const validateGetUser = celebrate({
 
 const validateUpdateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 });
 
 const validateUpdateAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(urlRegExp),
+    avatar: Joi.string().required().pattern(urlRegExp),
   }),
 });
 
