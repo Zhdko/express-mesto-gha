319af648130c46f53cd3244c0647123bcf188220
Alexandr Zhdanko <zhdko@yandex.ru>
2023-05-13 22:50:30 +0400

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,20 +4,39 @@ const { secretKey } = require('../utils/constants');
 require('dotenv').config();
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  const bearer = 'Bearer ';
 
-  if (!token) {
-    return next(new AuthorizationError('Необходима авторизация.'));
+  if (!authorization || !authorization.startsWith(bearer)) {
+    return next(new AuthorizationError('Необходима авторизация'));
   }
+
+  const token = authorization.replace(bearer, '');
 
   let payload;
   try {
     payload = jwt.verify(token, secretKey);
   } catch (err) {
-    return next(new AuthorizationError('Необходима авторизация.'));
+    return next(new AuthorizationError('Необходима авторизация'));
   }
 
   req.user = payload;
-
   return next();
+
+  // const token = req.cookies.jwt;
+
+  // if (!token) {
+  //   return next(new AuthorizationError('Необходима авторизация.'));
+  // }
+
+  // let payload;
+  // try {
+  //   payload = jwt.verify(token, secretKey);
+  // } catch (err) {
+  //   return next(new AuthorizationError('Необходима авторизация.'));
+  // }
+
+  // req.user = payload;
+
+  // return next();
 };
