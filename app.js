6edbb5620ebd5a0562741b09ef0bb796bc1d47d7
Alexandr Zhdanko <zhdko@yandex.ru>
2023-05-18 23:16:45 +0400

    fix: исправление README

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true })
 app.use(
   cors({
     credentials: true,
-    origin: 'https://mesto.zhdko.nomoredomains.monster',
+    origin: [
+      'https://mesto.zhdko.nomoredomains.monster',
+      'http://mesto.zhdko.nomoredomains.monster',
+
+    ],
     exposedHeaders: ['set-cookie'],
   }),
 );
