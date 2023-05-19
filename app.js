26cd241b9ae989b1eb6f28fabd082b180628dc8a
Alexandr Zhdanko <zhdko@yandex.ru>
2023-05-19 12:10:23 +0400

    styl:

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ mongoose.connect(DB, { useNewUrlParser: true }).catch((err) => console.log(err))
 app.use(
   cors({
     credentials: true,
-    origin: [
-      'https://mesto.zhdko.nomoredomains.monster',
-      'https://mesto.zhdko.nomoredomains.monster',
-    ],
+    origin: 'https://mesto.zhdko.nomoredomains.monster',
     exposedHeaders: ['set-cookie'],
   }),
 );
