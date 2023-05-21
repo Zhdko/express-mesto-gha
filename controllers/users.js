c067f4358fefc60dbd1337623a01d73ab50f65dc
Alexandr Zhdanko <zhdko@yandex.ru>
2023-05-21 21:46:43 +0400

    fix:смена разрешенных доменов

--- controllers/users.js
+++ controllers/users.js
@@ -51,7 +51,6 @@ const login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          sameSite: 'none',
           httpOnly: true,
         })
         .send({ token });
